0e63f70def4dba610a6b3a0c53270ed7bcae8433
V1rushB <211111@ppu.edu.ps>
2023-08-20 13:18:52 +0300

    Finializing

--- Imfrastructure/prepare-instance.sh
+++ Imfrastructure/prepare-instance.sh
@@ -9,9 +9,8 @@ curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 
 sudo apt install nodejs jq curl -y
 
-
 # deploy app
-repo="V1rushB/Booking-System-GSG-1"
+repo="mohammad-husaini/book-api"
 download_url=$(curl "https://api.github.com/repos/$repo/releases/latest" | jq --raw-output '.assets[0].browser_download_url')
 asset_name=$(curl "https://api.github.com/repos/$repo/releases/latest" | jq --raw-output '.assets[0].name')
 
@@ -20,6 +19,6 @@ sudo mv app.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable app.service
 
-sudo -u ubunto sh -c "mkdir -p /home/ubuntu/app && cd /home/ubuntu/app && curl -LO $download_url && tar xzvf app.tar.gz && npm install --omit=dev"
+sudo -u ubuntu sh -c "mkdir -p /home/ubuntu/app && cd /home/ubuntu/app && curl -LO $download_url && tar xzvf app.tar.gz && npm install --omit=dev"
 
-sudo reboot 
+sudo reboot
