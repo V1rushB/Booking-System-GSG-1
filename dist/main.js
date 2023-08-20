2422e990e33dc7530f0be4a1d2c6fab4fdcf549e
V1rushB <211111@ppu.edu.ps>
2023-08-20 14:17:17 +0300

    last commit

--- dist/main.js
+++ dist/main.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const book_js_1 = __importDefault(require("./router/book.js"));
-require("dotenv/config");
 const checker_js_1 = require("./middleware/checker.js");
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 2077;
